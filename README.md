bf1140993c3d8cf9a3ad6f544a5e577ba53788e9
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2020-12-29 20:03:13 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# VideoCompress
+A Telegram Video Compressor Bot By [@xgorn](https://t.me/xgorn). **This bot works for all!** No need to define each user IDs to use bot. Also works in Group.
+
+### Demo Bot:
+<a href="https://t.me/xvidcombot"><img src="https://img.shields.io/badge/Demo-Telegram%20Bot-blue.svg?logo=telegram"></a>
+
+## Easy Deploy:
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+### Projects Channel:
+<a href="https://t.me/xTeamBots"><img src="https://img.shields.io/badge/Telegram-Join%20Telegram%20Channel-blue.svg?logo=telegram"></a>
+
+## Required Configs:
+* `TG_BOT_TOKEN` - Get this from [@BotFather](https://t.me/BotFather)
+* `BOT_USERNAME` - Your Bot's Username which you send to [@BotFather](https://t.me/BotFather) while creating Bot. ***(Without `@` Before Username!!)***
+* `APP_ID` - Get this from my.telegram.org
+* `API_HASH` - Get this from my.telegram.org
+* `AUTH_USERS` - Put your ID & other Sudo Users IDs. Separate with **Space**. Just for using ***Admin Commands***.
+
+### Admin Commands:
+```
+log - Bot Will Send it's Logs File.
+cancel - Bot will Cancel Last Process.
+```
+
+
+### Credits:
+* [Jijinr](https://github.com/Jijinr)
+* [SpEcHide](https://github.com/spechide)
+* [AbirHasan2005](https://github.com/AbirHasan2005)
---
+++ bot/localisation.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from bot.get_cfg import get_config
+
+
+class Localisation:
+    START_TEXT = "Hi, \nI'm X-Video-Compressor \nYou can Compress Telegram Videos \n\n/help for more details."
+   
+    ABS_TEXT = " Please don't be selfish."
+    
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    
+    
+    DOWNLOAD_START = "📥 Downloading 📥 \n"
+    
+    UPLOAD_START = "📤 Uploading 📤 \n"
+    
+    COMPRESS_START = "📀 Trying to compress ... 📀"
+    
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+    
+    COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n📀 Compressed in {}\n\n📤 Uploaded in {}"
+
+    COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
+
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+    
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    
+    SAVED_RECVD_DOC_FILE = "✅ Downloaded Successfully."
+    
+    CUSTOM_CAPTION_UL_FILE = " "
+    
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    
+    NO_VOID_FORMAT_FOUND = "no-one gonna help you\n{}"
+    
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️ Already One Process going on! ⚠️"
+    
+    HELP_MESSAGE = get_config(
+        "STRINGS_HELP_MESSAGE",
+        "How to Use Me? Follow These steps! \n\n1. Send me your Telegram Video. \n2. Reply the file - /compress And Persentage \nEg: <code>/compress 50</code> \n\nIf bot didn't respond, contact @xgorn"
+    )
+    WRONG_MESSAGE = get_config(
+        "STRINGS_WRONG_MESSAGE",
+        "current CHAT ID: <code>{CHAT_ID}</code>"
+    )
+
+    
